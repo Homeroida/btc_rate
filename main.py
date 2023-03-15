d30b22004122cc8ea2795b5fd274ae396b8bd18e
Zurab <chanishvili@gmail.com>
2023-03-15 21:36:11 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+try:
+    n = sys.argv[1]
+    # conveer to float
+    n = float(n)
+
+    # sending requests for data
+    response = requests.get(
+        'https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+
+    # formating json file
+    rate = data['bpi']['USD']['rate']
+
+    # conveer to float
+    formated_rate = float(rate.replace(',', ''))
+
+    result = formated_rate * n
+
+    print(f'${result:,.4f}')
+
+except ValueError:
+    print('Command-line argument is not a number')
+
+except IndexError:
+    print('Missing command-line argument')
